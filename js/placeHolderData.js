49fd79c7de8f2df5aa65a6165c27cc731f98ffee
Tanbir Ahmed Mazumdar <46240243+tanbir-create@users.noreply.github.com>
2022-03-11 15:37:45 +0530

    Add files via upload

---
+++ js/placeHolderData.js
@@ -0,0 +1,113 @@
+//REMOVE FROM HERE AND HTML FILE IN PRODUCITON
+
+
+
+let astro = {
+
+
+    name: 'Deepak Jain',
+    rating: '4.56',
+    followers: '9.2',
+    served: '14.1',
+    exp: '17',
+    audio: 'https://www.guruji.app/public/static/images/master/159/voice.aac',
+    languages: ['Hindi', 'English', 'Punjabi'],
+    skills: [],
+    images: ['images/upcoming2.jpeg', 'images/astro-deepak.png', 'images/astro-neelima.png', 'images/upcoming3.jpg', 'images/astro-deepak.png', 'images/astro-neelima.png'],
+    description: 'Deepak Kumar Jain is the founder of Deepak Astro Vaastu. Hes been practicing since 2004. He started teaching these divine sciences in 2008 and has educated more than 500 students and the journey continues. He also practiced and mastered the Prashan Shastra. Gradually he experienced that we can only improve the things and empower our karmas. If we focus on our karma and take the help of astrology, vaastu and other divine sciences, we can get good results. So he started analysing the horoscope to its purpose, know-yourself with the help of Astrology. People feel happy when they know the Purpose of their Life. He then researched on Lal kitab and Vaastu Remedies. During this research he found that even we can get the help of Ayurveda and Yoga as a Remedial Part and it works well. He works like a scientist, have logic of his every remedy and prediction. He works on patients suffering from chronic diseases and helps them to cure rapidly.'
+
+
+}
+
+
+
+
+let reviews = {
+
+
+    total: 87355,
+    five: 68888,
+    four: 5546,
+    three: 7256,
+    two: 4000,
+    one: 1665
+
+}
+
+
+
+let userReview = [
+
+    {
+        username: 'Reviewer Name',
+        date: '2days ago',
+        image: 'images/astro-image.png',
+        stars: 3,
+        
+        review: 'Might as well get right to it, then. At the risk of sounding like a contrarian, I did not love this film. Do I love elements of this? Yes.  did not love this fi did not love this fi did not love this fi did not love this fi did not love this fi did not love this fi',
+        
+        astroFeedback: 'The last time, I felt like this, in a cinema, I was six years old and I was watching Star Wars. I never imagined, I would ever find that feeling again in a cinema. That sense of being transported to another world. '
+    },
+
+    {
+        username: 'Reviewer Name',
+        date: '2days ago',
+        
+        stars: 1,
+
+        review: 'Might as well get right to it, then. At the risk of sounding like a contrarian, I did not love this film. Do I love elements of this? Yes.  did not love this fi did not love this fi did not love this fi did not love this fi did not love this fi did not love this fi',
+        
+       
+    },
+
+    {
+        username: 'Reviewer Name',
+        date: '2days ago',
+        image: 'images/astro-image.png',
+        stars: 5,
+        
+        review: 'Might as well get right to it, then. At the risk of sounding like a contrarian, I did not love this film. Do I love elements of this? Yes.  did not love this fi did not love this fi did not love this fi did not love this fi did not love this fi did not love this fi',
+        
+        astroFeedback: 'The last time, I felt like this, in a cinema, I was six years old and I was watching Star Wars. I never imagined, I would ever find that feeling again in a cinema. That sense of being transported to another world. '
+        
+        
+    },
+
+
+]
+
+
+
+
+let posts = [
+
+
+    {
+        date: '1day ago',
+        image: 'images/astro-deepak.png',
+        likes: '1252'
+        
+    },
+    {
+        date: '3days ago',
+        video: 'images/Astro-deepak.mp4',
+        title: 'Introducing Astro Deepak',
+        likes: '554',
+        
+    },
+
+    {
+        date: '5days ago',
+        text: 'Deepak Kumar Jain is the founder of Deepak Astro Vaastu. Hes been practicing since 2004. He started teaching these divine sciences in 2008 and has educated more than 500 students and the journey continues. He also practiced and mastered the Prashan Shastra. Gradually he experienced that we can only improve the things and empower our karmas. If we focus on our karma and take the help of astrology, vaastu and other divine sciences, we can get good results. So he started analysing the horoscope to its purpose, know-yourself with the help of Astrology. People feel happy when they know the Purpose of their Life. He then researched on Lal kitab and Vaastu Remedies. During this research he found that even we can get the help of Ayurveda and Yoga as a Remedial Part and it works well. He works like a scientist, have logic of his every remedy and prediction. He works on patients suffering from chronic diseases and helps them to cure rapidly.',
+        likes: '745'
+    },
+    {
+        date: '4months ago',
+        isArticle: true,
+        title: 'Title of article',
+        likes: '12',
+        image: 'images/astro-deepak.png'
+    }
+
+
+]
